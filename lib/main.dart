d5792387d0cabc4778252c726b77f457f6d711a1
BH-mk2 <abesan4@gmail.com>
2024-02-24 00:07:56 +0900

    Todoに必要なデータ構造体とウィジェットを別ファイルに分離

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'dialog/inputdialog_todo.dart';
 
-class stTodo {
-  String id = '';
-  String taskName = '';
-  bool isCheck=false;
-
-  stTodo({required String id, required String taskName, required bool isCheck}){
-    this.id = id;
-    this.taskName = taskName;
-    this.isCheck = isCheck;
-  }
-}
+import 'todo.dart';
 
 // TODOアプリ
 // TODOの追加・削除・チェックの付け外しが出来る
@@ -78,9 +68,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
                   itemCount: widget.todoList.length,
                   itemBuilder: (BuildContext context, index) {
                     return TodoTile(
-                      id: widget.todoList[index].id,
-                      taskName: widget.todoList[index].taskName,
-                      isCheck: widget.todoList[index].isCheck,
+                      todo: widget.todoList[index],
                       onDismissed: (direction) {
                         setState(() {
                           widget.todoList.removeAt(index);
@@ -123,71 +111,6 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
         },
         tooltip: 'Add Todo',
         child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class TodoTile extends StatefulWidget {
-  TodoTile({
-    Key? key,required this.id, required this.taskName, required this.isCheck,
-    required this.onDismissed
-  }) : super(key: key);
-
-  String id;
-  String taskName;
-  bool isCheck;
-  final DismissDirectionCallback onDismissed;
-
-  @override
-  _TodoTileState createState() => _TodoTileState();
-}
-
-class _TodoTileState extends State<TodoTile> {
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(widget.id),
-      onDismissed: (direction) => widget.onDismissed(direction),
-      child: Padding(
-        padding:const EdgeInsets.all(8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Checkbox(
-                value: widget.isCheck,
-                onChanged: (bool? state){
-                  setState(() {
-                      widget.isCheck = state ?? false;
-                  });
-                }
-            ),
-            Expanded(
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                      // borderRadius: BorderRadius.circular(10)
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(0),
-                          bottomLeft: Radius.circular(0),
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        )
-                    )
-                  ),
-                  child: Text(widget.taskName),
-                  onPressed: () {
-                    setState(() {
-                      widget.isCheck = !widget.isCheck;
-                    });
-                  },
-                ),
-              ),
-            ),
-          ]
-        )
       ),
     );
   }
